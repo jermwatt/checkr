362542692e9bddb0841325a17cf204a5c4e8ced8
Jeremy Watt <jeremywatt@Jeremys-MacBook-Air.local>
2024-05-08 09:47:15 -0700

    ruff toml and actions added

--- setup.py
+++ setup.py
@@ -5,35 +5,33 @@ setup(
     name="meme_checkr",
     version="1.0.0",
     description="a hello world example",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Jeremy Watt",
     email="jermwattml@gmail.com",
     url="https://github.com/jermwatt/meme_checkr",
     packages=find_packages(include=["meme_checkr*"]),
     python_requires=">=3.8",
-    entry_points={
-        "console_scripts": [ 
-            "hello-world-cli = meme_checkr.main:main"
-            ]
-    },
+    entry_points={"console_scripts": ["hello-world-cli = meme_checkr.main:main"]},
     install_requires=[
-            "boto3",
-            "ffmpeg-python",
-            "moviepy",
-            "nltk",
-            "numpy",
-            "pillow",
-            "pyaml",
-            "pydub",
-            "pypdf",
-            "pytest",
-            "pytest-subtests",
-            "python-docx",
-            "python-dotenv",
-            "python-pptx",
-            "requests"
+        "boto3",
+        "ffmpeg-python",
+        "moviepy",
+        "nltk",
+        "numpy",
+        "pillow",
+        "pyaml",
+        "pydub",
+        "pypdf",
+        "pytest",
+        "pytest-subtests",
+        "python-docx",
+        "python-dotenv",
+        "python-pptx",
+        "requests",
     ],
     extras_require={
-        'linting': [
+        "linting": [
             "pylint",
             "mypy",
             "typing-extensions",
@@ -42,6 +40,6 @@ setup(
         "testing": [
             "pytest",
             "pytest-xdist",
-            ]
-    }
+        ],
+    },
 )
