4a3b432495d637ca44c26f53930789c90b395667
Jeremy Watt <jeremywatt@Jeremys-MacBook-Air.local>
2024-05-08 09:16:02 -0700

    test setup push

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="meme_checkr",
+    version="1.0.0",
+    description="a hello world example",
+    author="Jeremy Watt",
+    email="jermwattml@gmail.com",
+    url="https://github.com/jermwatt/meme_checkr",
+    packages=find_packages(include=["meme_checkr*"]),
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [ 
+            "hello-world-cli = meme_checkr.main:main"
+            ]
+    },
+    install_requires=[
+            "boto3",
+            "ffmpeg-python",
+            "moviepy",
+            "nltk",
+            "numpy",
+            "pillow",
+            "pyaml",
+            "pydub",
+            "pypdf",
+            "pytest",
+            "pytest-subtests",
+            "python-docx",
+            "python-dotenv",
+            "python-pptx",
+            "requests"
+    ],
+    extras_require={
+        'linting': [
+            "pylint",
+            "mypy",
+            "typing-extensions",
+            "ruff",
+        ],
+        "testing": [
+            "pytest",
+            "pytest-xdist",
+            ]
+    }
+)
